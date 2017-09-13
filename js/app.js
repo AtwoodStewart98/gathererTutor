e770b4b9d6c6be61c6d0d27df32150f8a63dd1b2
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2017-09-13 11:05:19 -0500

    installed router for multiple pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
-angular.module("deckbrew", []).config(function($sceDelegateProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    "self",
-    "https://api.deckbrew.com/mtg/cards"
-  ]);
-});
+angular
+  .module("deckbrew", ["ui.router"])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "../views/selectors.html"
+      })
+      .state("tutor", {
+        url: "/search-results/:id",
+        templateUrl: "../views/search-results.html"
+      })
+      .state("library", {
+        url: "/my-libraries",
+        templateUrl: "../views/library.html"
+      });
+  })
+  .config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      "self",
+      "https://api.deckbrew.com/mtg/cards"
+    ]);
+  });
--- js/deckbrewSrvc.js
+++ js/deckbrewSrvc.js
@@ -1,5 +1,4 @@
 angular.module("deckbrew").service("deckbrewSrvc", function($http) {
-  //this.gathererInfo = deckbrewDir.data;
   this.gathererInfo = function() {
     return $http.get("https://api.deckbrew.com/mtg/cards?page=0");
   };
